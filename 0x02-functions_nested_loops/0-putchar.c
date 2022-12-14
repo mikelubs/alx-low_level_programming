61cf5085010aa0a524570fe402513879d9301fcc
Lubembe Michael <michaelubembe@gmail.com>
2022-12-14 14:53:08 -0800

    removed trailing whitespace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - main block
+ * main - main function block
  * Description : Print `_putchar` then a new line
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar("\n");
 	return (0);
 }
