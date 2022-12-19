19ea595debcf46363da5cbdd05f96c8e48cd125d
Lubembe Michael <michaelubembe@gmail.com>
2022-12-19 14:44:43 -0800

    print length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - length of the string s
+ * @s: pointer to a char
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++);
+
+	return (a);
+}
