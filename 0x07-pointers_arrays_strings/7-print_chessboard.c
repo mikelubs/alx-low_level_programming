6079302d2b7b7c546e0839a60b305bfc49b5649d
Lubembe Michael <lubemb@gmail.com>
2023-01-03 12:15:12 -0800

    print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	j = 0;
+	while (j < 8)
+	{
+		i = 0;
+		while (i < 8)
+		{
+			_putchar(a[j][i]);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
