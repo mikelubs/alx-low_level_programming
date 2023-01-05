7a12acbfe21a10e8648dc39df487b47e9a67dfa6
Lubembe Michael <lubemb@gmail.com>
2023-01-05 14:16:52 -0800

    palindrome check

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: char array string
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = get_l(s) - 1;
+
+	return (my_pal(s, --len));
+}
+
+/**
+ * get_l - gets length of string
+ * @s: string
+ * Return: return length of string
+ */
+
+int get_l(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (1 + get_l(++s));
+}
+
+/**
+ * pal_check - recursive check of palindrome
+ * @s: string
+ * @l: length of string
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int pal_check(char *s, int l)
+{
+	if (*s == *(s + l))
+	{
+		if (l <= 0)
+			return (1);
+		else
+			return (pal_check(++s, l - 2));
+	}
+	else
+		return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,5 @@ int hsqrt(int n, int d);
 int sq_rt(int x, int i);
 int sol(int c, int i);
 int is_palindrome(char *s);
+int pal_check(char *s, int l);
+int get_l(char *s);
